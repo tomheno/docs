21721ba281fb404d36686dee77470842b2344f6d
Ryan Scherler <ryan@eastslope.studio>
2021-02-19 16:43:15 -0800

    .

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -6,15 +6,15 @@
         <nav class="px-6 py-12 lg:py-24">
             <ol class="max-w-screen-2xl mx-auto relative flex justify-between md:text-lg">
                 <li class="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <a href="/" rel="home" class="transition-colors duration-200 hover:text-blue-700">
+                    <a href="/" rel="home" class="transition-colors duration-200 hover:text-primary-700">
                         <x-logo :alt="$page->siteName" class="w-36 h-auto" />
                     </a>
                 </li>
-                <li><a href="#" class="hover:text-blue-700 hover:underline transition-colors duration-200">Documentation</a></li>
+                <li><a href="#" class="hover:text-primary-700 transition-colors duration-200">Documentation</a></li>
                 <li>
                     <a 
                         href="{{ $page->projectUrl }}" 
-                        class="hover:text-blue-700 transition-colors duration-200"
+                        class="hover:text-primary-700 transition-colors duration-200"
                         target="_blank"
                         rel="noreferrer noopener"
                     >
