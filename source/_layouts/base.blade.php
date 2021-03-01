c9ceaf6c49265adacbc93cbcd49203ac4f540134
Ryan Scherler <ryan@eastslope.studio>
2021-02-28 23:04:24 -0800

    wip

--- source/_layouts/base.blade.php
+++ source/_layouts/base.blade.php
@@ -24,7 +24,40 @@
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script src="{{ mix('js/main.js', 'assets/build') }}" defer></script>
     </head>
-    <body class="font-sans antialiased font-light text-white">
-        @yield('body')
+    <body class="font-sans antialiased font-light text-gray-700 bg-white">
+        <header role="banner">
+            @yield('header')
+        </header>
+        <main role="main">
+            @yield('main')
+        </main>
+        <footer role="contentinfo" class="relative px-6 py-8 text-center text-white bg-primary-900 md:py-16">
+            <img src="/assets/media/bg-illustrations@2x.jpg" alt="Illustrations" class="absolute top-0 left-0 object-cover w-full h-full opacity-20" />
+            <div class="relative max-w-screen-sm mx-auto space-y-2 md:flex md:justify-between md:space-y-0 md:space-x-8">
+                <p>
+                    Built with 
+                    <a href="https://jigsaw.tighten.co" target="_blank" rel="noopener noreferrer" class="font-medium underline transition-colors duration-200 hover:text-primary-200">Jigsaw</a> 
+                    &amp; 
+                    <a href="https://tailwindcss.com" target="_blank" rel="noopener noreferrer" class="font-medium underline transition-colors duration-200 hover:text-primary-200">Tailwind CSS</a>.
+                </p>
+    
+                <ul class="inline-flex space-x-2">
+                    <li>
+                        <a 
+                            href="{{ $page->projectUrl }}" 
+                            class="font-medium underline transition-colors duration-200 hover:text-primary-200"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                        >
+                            Github
+                        </a>
+                    </li>
+                    <li aria-hidden="true">&bull;</li>
+                    <li>
+                        <a href="/docs" class="font-medium underline transition-colors duration-200 hover:text-primary-200">Documentation</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
     </body>
 </html>
--- source/_layouts/documentation.blade.php
+++ source/_layouts/documentation.blade.php
@@ -1,9 +1,18 @@
 @extends('_layouts.base')
 
-@section('body')
-    <div class="container p-4 bg-white">
-        <div class="prose lg:prose-lg">
-            @yield('content')
+@section('header')
+    header
+@endsection
+
+@section('main')
+    <div class="max-w-screen-xl px-4 py-6 mx-auto lg:px-6 lg:grid lg:grid-cols-5 lg:gap-10">
+        <nav>
+            nav
+        </nav>
+        <div class="lg:col-span-3">
+            <div class="prose lg:prose-lg">
+                @yield('content')
+            </div>
         </div>
     </div>
 @endsection
