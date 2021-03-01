d9767f053f643fb1fe0d2ab382aefc077ad25074
Ryan Scherler <ryan@eastslope.studio>
2021-03-01 12:40:32 -0800

    meta desc

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'production' => false,
     'baseUrl' => '',
     'siteName' => 'Filament',
-    'siteDescription' => 'The elegant TALL stack admin panel for Laravel artisans.',
+    'siteDescription' => 'Filament is a content management framework for rapidly building a beautiful administration interface designed for humans.',
     'projectUrl' => 'https://github.com/laravel-filament/filament',
     'collections' => [],
     'navigation' => require_once('navigation.php'),
